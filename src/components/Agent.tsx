22a49f63a81ba3b7269d0220bbe1032167b75cf1
netizen-om <borisagarom@gmail.com>
2025-07-08 11:15:51 +0530

    trying to conver tailwindv3 to v4

--- src/components/Agent.tsx
+++ src/components/Agent.tsx
@@ -1,14 +1,46 @@
+import { Span, } from 'next/dist/trace';
 import React from 'react'
+import Image from "next/image";
 
-const Agent = () => {
+const Agent = ({userName} : AgentProps) => {
+
+    const isSpeaking = true;
+
   return (
-    <div className="call-view">
-        <div className='card-interviewer'>
-            <div className="avatar">
-                <img src="/ai-avatar.png" alt="vapi" width={65} height={54}/>
-            </div>
+    <>
+        <div className="call-view">
+        {/* AI Interviewer Card */}
+        <div className="card-interviewer">
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              alt="profile-image"
+              width={65}
+              height={54}
+              className="object-cover"
+            />
+            {isSpeaking && <span className="animate-speak" />}
+          </div>
+          <h3>AI Interviewer</h3>
         </div>
-    </div>
+
+        {/* User Profile Card */}
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src="/user-avatar.png"
+              alt="profile-image"
+              width={1000}
+              height={539}
+              className="rounded-full object-cover size-[120px]"
+            />
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+
+    </>
+    
   )
 }
 
