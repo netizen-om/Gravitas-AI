92509dfb8f9ea96b4d2928a94b9e8d83c462f9e0
netizen-om <borisagarom@gmail.com>
2025-08-24 15:01:55 +0530

    added route to update profile picture

--- pravya/src/app/api/user/update-coverImage/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
-import { prisma } from "@/lib/prismadb";
-import cloudinary from "@/lib/cloudinary";
-
-export async function POST(req : Request) {
-
-}
